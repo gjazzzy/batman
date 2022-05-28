9ed832d4b77c25a2df76785b29daf346dd5f6000
Григорий <89094284+gjazzzy@users.noreply.github.com>
2022-05-28 15:51:47 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $('.slider').slick({
     arrows: false, //Вкл стрелок
     adaptiveHeight: true, // Адаптивные точки
-    fade:true,
     slidesToShow: 2, // Показ  кол-вл слайдов
     slidesToScroll: 1, // Скролл слайдов
     speed: 1000, // Скорость перелистываться анимации
@@ -15,30 +14,36 @@ $(document).ready(function () {
     pauseOnDotsHover: true, // Пауза при наведении на Dots
     pauseOnHover: true, // Пауза при наведении на слайд
     draggable: true, // Нельзя на ПК! свайпить
-    swipe: true, // Нельзя на тачскринах свайпить
+    swipe: false, // Нельзя на тачскринах свайпить
     touchThreshold: 7, // Момент перелистыванья
     touchMove: true, // Ручное перелистыванье выкл
     waitForAnimate: true, // Быстрая перекрутка
-    centerMode: false, // Центральный слайд
+    centerMode: true, // Центральный слайд
     variableWidth: false, // Авто-Адаптивный слайдер 
     rows: 1, // Колонк
     vertical: false, // Вертикальный слайдер (без использовать с d:block)
 
-  // appendArrows:$('.button-nav') // Расположение стрелок в отдельном меню 
+
+    
   
-  // responsive:[ // брекПоинт и его настройка
-  //   {
-  //     breakpoint: 500,
-  //     settings: {
-  //       slidesToShow:2
-  //     }
-  //   }, {
-  //      breakpoint: 900,
-  //     settings: {
-  //       slidesToShow: 2
-  //     }
-  //   }
-  //   ],
+  responsive:[ // брекПоинт и его настройка
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow:1,
+        centerMode: true, // Центральный слайд
+        variableWidth: true, // Авто-Адаптивный слайдер
+          touchThreshold: 1, // Момент перелистыванья
+   
+      }
+    }, 
+    // {
+    //    breakpoint: 900,
+    //   settings: {
+    //     slidesToShow: 2
+    //   }
+    // }
+    ],
   // mobileFirst: true,// miv-width !
   });
 });
